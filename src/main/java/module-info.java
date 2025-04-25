801ec40d50a429c49274012c4c10dd8332a0e9ea
Sunisima <lsun8525@gmail.com>
2025-04-25 08:47:50 +0200

    Forfra da andet repository ikke virkede

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,6 @@ module org.example.canteen2 {
 
     opens org.example.canteen2 to javafx.fxml;
     exports org.example.canteen2;
+    exports org.example.canteen2.Controllers;
+    opens org.example.canteen2.Controllers to javafx.fxml;
 }
--- src/main/java/org/example/canteen2/HelloApplication.java
+++ src/main/java/org/example/canteen2/HelloApplication.java
@@ -2,21 +2,78 @@ package org.example.canteen2;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.example.canteenmgmtsystem.controllers.SceneControllerPairs;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 
 public class HelloApplication extends Application {
+    private static Collection<SceneControllerPairs> scenes = new ArrayList<>();
+    private static Stage primaryStageHolder = null;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("LoginView.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
+        //Application.setUserAgentStylesheet(getClass().getResource("Style.css").toExternalForm());
+
+        primaryStageHolder = stage;
+
+        //Loads our LoginView.fxml as our first Scene
+        FXMLLoader LoginViewLoader = new FXMLLoader(getClass().getResource("LoginView.fxml"));
+        Parent LoginViewPane = LoginViewLoader.load();
+        Scene overViewScene = new Scene(LoginViewPane, 800, 600);
+
+
+        scenes.add(new SceneControllerPairs(
+                new Lazy<>(() -> buildScene("LoginView.fxml")),
+                ControllerNames.LoginView
+        ));
+
+        scenes.add(new SceneControllerPairs(
+                new Lazy<>(() -> buildScene("MenuView.fxml")),
+                ControllerNames.MenuView
+        ));
+
+        scenes.add(new SceneControllerPairs(
+                new Lazy<>(() -> buildScene("ChoosePaymentView.fxml")),
+                ControllerNames.PaymentView
+        ));
+
+
+        stage.setTitle("Green Canteen");
+        stage.setScene(overViewScene);
         stage.show();
     }
 
+    /**
+     * changeScene
+     * @param sceneName
+     */
+    public static void changeScene(ControllerNames sceneName) {
+        primaryStageHolder.setTitle(sceneName.toString());
+
+        for (SceneControllerPairs sceneCtrlNamePair : scenes) {
+            if (sceneCtrlNamePair.getName().equals(sceneName)) {
+                primaryStageHolder.setScene(sceneCtrlNamePair.getScene());
+            }
+        }
+    }
+
+    private Scene buildScene(String resource) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource(resource));
+            Parent root = loader.load();
+            Scene scene = new Scene(root, 800, 600);
+            //scene.getStylesheets().add(getClass().getResource(cssResource).toExternalForm());
+            return scene;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
     public static void main(String[] args) {
         launch();
     }
