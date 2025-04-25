1994766adb61ccde7179b34db9013a94afcbd89b
Niklas <jan-niklas-bremer@web.de>
2025-04-25 10:13:57 +0200

    Addad MenuItems and order, comments on updateStorage().

--- src/main/java/org/example/canteen2/Models/Order.java
+++ src/main/java/org/example/canteen2/Models/Order.java
@@ -1,4 +1,23 @@
 package org.example.canteen2.Models;
 
-public class Order {
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order
+{
+    private List<MenuItem> orderItems = new ArrayList<>();
+
+    public void addItem(MenuItem item)
+    {
+        orderItems.add(item);
+        System.out.println(item.getName() + " added to order.");
+    }
+
+    public void updateStorage()
+        {
+
+            //Takes the items added into the ArrayList and makes subtracts them from the database with
+            //the help of Stored Procedures and callable Statements.
+        }
 }
